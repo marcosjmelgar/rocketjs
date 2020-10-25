d3de9e5ad346aaf4eabf52a906cc9d9056d107c8
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-25 20:46:01 -0300

    PS-04: "exercise-3-a"

--- excersice2/js/exercise2.js
+++ excersice2/js/exercise2.js
@@ -3,7 +3,6 @@
 var x1 = "hello world my name is marcos melgar";
 var x2 = x1.toUpperCase();
 console.log(x2);
-alert (x2);
 
 /*Crear una variable de tipo string con al menos 10 caracteres
  y convertir todo el texto en mayúscula (utilizar toUpperCase).*/
@@ -11,7 +10,6 @@ alert (x2);
  var x1 = "hello world my name is marcos melgar";
  var x2 = x1.substring(5,0);
  console.log (x2);
- alert (x2);
 
  /*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
  caracteres guardando el resultado en una nueva variable (utilizar substring).*/
@@ -19,7 +17,6 @@ alert (x2);
  var x1 = "hello world my name is undefined";
  var x2 = x1.substring(29);
  console.log (x2);
- alert (x2);
 
  /*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3 
  caracteres guardando el resultado en una nueva variable (utilizar substring).*/
@@ -29,7 +26,6 @@ alert (x2);
  var x3 = x1.substring(1).toLowerCase();
  var x4 = x2 + x3;
  console.log(x4);
- alert (x4);
 
 /*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
 string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado 
@@ -38,7 +34,6 @@ en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operado
 var x1 = "hello world ";
 var x2 = x1.indexOf(" ");
 console.log(x2);
-alert (x2);
 
 /*Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del \
 primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
@@ -54,7 +49,6 @@ var x8 = x1.substring(x7,20).toLowerCase();
 var x9 = x6 + x8;
 var x10 = x4 + x9;
 console.log(x10);
-alert (x10);
 
 /*Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio 
     entre medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo 
---
+++ excersice2/js/exercise3.js
@@ -0,0 +1,11 @@
+/*Arrays*/
+
+var meses= ["Enero","Febrero", "Marzo", "Abril", "Mayo","Junio",
+"Julio","Agosto","Septiembre", "Octubre","Noviembre","Diciembre"];
+console.log (meses[4],meses[10]);
+
+/*Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+ mostrar por consola los meses 5 y 11 (utilizar console.log)*/
+
+ 
