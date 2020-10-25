f123a203f00d4b4ede2fee3bfe659af9b4541541
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-25 14:10:12 -0300

    PS-04: "exercise-2-b"

--- excersice2/js/exercise2.js
+++ excersice2/js/exercise2.js
@@ -5,4 +5,13 @@ var x2 = x1.toUpperCase();
 console.log(x2);
 alert (x2);
 
+/*Crear una variable de tipo string con al menos 10 caracteres
+ y convertir todo el texto en mayúscula (utilizar toUpperCase).*/
 
+ var x1 = "hello world my name is marcos melgar";
+ var x2 = x1.substring(5,0);
+ console.log(x2);
+ alert (x2);
+
+ /*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
+ caracteres guardando el resultado en una nueva variable (utilizar substring).*/
