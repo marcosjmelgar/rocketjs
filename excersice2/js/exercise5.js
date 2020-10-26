c15989cdda68b54ffd26c462e9dd85a876026909
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-26 20:52:41 -0300

    PS-04: "exercise-5-d"

--- excersice2/js/exercise5.js
+++ excersice2/js/exercise5.js
@@ -25,3 +25,15 @@ alert(sentence);
 /*Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto 
 a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. 
 Al final mostrar una única alerta con la cadena completa.*/
+
+var vacio = [];
+for (var index=0; index<10; index++) {
+    vacio [index] = index;
+}
+console.log(vacio);
+
+/*Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, 
+es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, 
+desde el número 0 hasta al numero 9. Mostrar por la consola del navegador el al array final 
+(utilizar console.log).*/
+
