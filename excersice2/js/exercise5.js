007a06f3d3f1ead200a248fe98ea1779abbc4a70
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-25 23:14:44 -0300

    PS-04: "exercise-5-b"

--- excersice2/js/exercise5.js
+++ excersice2/js/exercise5.js
@@ -1,9 +1,17 @@
 /*For*/
 
-var dias = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
+var dias = ["lunes","martes","miercoles","jueves","viernes"]
 for (var index = 0; index<dias.length; index++) {
     console.log(dias[index]);
 }
 
 /*Crear un array que contenga 5 palabras y recorrer dicho array utilizando 
 un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
+
+for (var index=0; index<dias.length; index++) {
+    dias[index] = dias[index].charAt(0).toUpperCase() + dias[index].substring(1);
+    console.log(dias);
+}
+
+/*Al array anterior convertir la primera letra de 
+cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
