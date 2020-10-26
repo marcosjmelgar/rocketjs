731bf20f23adfae63aae4109f71ceb3789cfb2d3
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-26 20:32:24 -0300

    PS-04: "exercise-5-c"

--- excersice2/js/exercise5.js
+++ excersice2/js/exercise5.js
@@ -15,3 +15,13 @@ for (var index=0; index<dias.length; index++) {
 
 /*Al array anterior convertir la primera letra de 
 cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.*/
+
+var sentence = ""
+for (var index=0; index<dias.length; index++) {
+    sentence = sentence + dias[index];
+}
+alert(sentence);
+
+/*Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto 
+a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. 
+Al final mostrar una única alerta con la cadena completa.*/
