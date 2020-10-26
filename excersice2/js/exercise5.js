bbd8412dce21c67d05752816158c304231191392
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-25 22:55:35 -0300

    PS-04: "exercise-5-a"

---
+++ excersice2/js/exercise5.js
@@ -0,0 +1,9 @@
+/*For*/
+
+var dias = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
+for (var index = 0; index<dias.length; index++) {
+    console.log(dias[index]);
+}
+
+/*Crear un array que contenga 5 palabras y recorrer dicho array utilizando 
+un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
