2ade3426a059be3c33755a41e0cd4cff809a2d0b
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-10-26 21:11:05 -0300

    PS-04: "exercise-6-a"

---
+++ excersice2/js/exercise6.js
@@ -0,0 +1,11 @@
+/*Functions*/
+
+function suma (x1,x2) {
+    return x1 + x2 ;
+}
+resultado = suma(3,3);
+console.log(resultado);
+
+/*Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y 
+guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
+
