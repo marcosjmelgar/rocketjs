4b884bed7172d1f923bfdbff2237c7aa79f48e50
marcosjmelgar <72085792+marcosjmelgar@users.noreply.github.com>
2020-11-02 23:47:38 -0300

    PS-05: "creating-alerts-with-focus"

--- excersice1/js/form.js
+++ excersice1/js/form.js
@@ -12,32 +12,34 @@
  //var name = document.getElementsByClassName("fullname");
 
 window.onload = function(){
+    var fullname = document.getElementById ("fullname");
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var rpassword = document.getElementById("rpassword");
     var age = document.getElementById("age");
-    var telephone = document.getElementsByClassName("telephone");
-    var adress = document.getElementsByClassName("adress");
-    var city = document.getElementsByClassName("city");
-    var postal = document.getElementsByClassName("postal");
-    var identification = document.getElementsByClassName("identification");
-    var fullname = document.getElementById ("fullname");
+    var telephone = document.getElementById("telephone");
+    var adress = document.getElementById("adress");
+    var city = document.getElementById("city");
+    var postal = document.getElementById("postal");
+    var identification = document.getElementById("identification");
 
-    fullname.onblur = validateName
+    fullname.onblur = validateName;
+    fullname.onfocus = hideDiv;
+
 
     function validateName () {
         if ( fullname.value.length <= 6  || !fullname.value.includes(" ")) {
-            console.log("Name lower than 6 letters or withouth space");
+            document.getElementById ("fullnameoutput").style.visibility = "visible";
         }
     }
 
     email.onblur = validateEmail;
+    email.onfocus = hideDiv;
 
     function validateEmail () {
         if (/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/.test(email.value)) {
-            //console.log("correo aceptado");
         } else {
-                //console.log("correo no aceptado");
+                document.getElementById ("emailoutput").style.visiblity = "visible";
             }
     }
 
@@ -62,12 +64,75 @@ window.onload = function(){
     }
 
     age.onblur = validateAge;
-// falta validar que sea entero
+
     function validateAge () {
         if (age.value.search (/\d/) == -1) {
             console.log ("type a number");
-        } else if ( age.value < 18) {
-            console.log("age must be older than 18");
-        } else if (console.log ("age ok"));
+        } else if ( age.value < 18 || age.value % 1 !== 0) {
+            console.log("age must be older than 18 and to be an integer");
+        } else ("age ok");
+    }
+
+    telephone.onblur = validateTelephone;
+
+    function validateTelephone () {
+        if (telephone.value.search (/\d/) == -1 ) {
+            console.log ("type a number");
+        } else if (telephone.value.search (/\s/) !== -1) {
+            console.log ("error blank");
+        }   else if (telephone.value.search (/\(/) !== -1 || telephone.value.search (/\)/) !== -1 ) {
+            console.log ("parentesis error");
+        }   else if (telephone.value.search (/\-/) !== -1) {
+            console.log ("guion error");
+        } else if (telephone.value.length < 7) {
+            console.log ("telephone at least 7 numbers");
+        }
+    }
+
+    adress.onblur = validateAdress;
+
+    function validateAdress () {
+        if ( adress.value.length < 5) {
+            console.log ("adress less than 5 chracters");
+        }   else if (adress.value.search (/\w\s\d/) == -1) {
+        console.log("ok")
+        }
+    }
+
+    city.onblur = validateCity;
+
+    function validateCity () {
+        if (city.value.length < 3) {
+            console.log("city less than 3 characters")
+        } else {
+            console.log ("ok")
+        }
+    }
+
+    postal.onblur = validatePostal;
+
+    function validatePostal () {
+        if ( postal.value.length < 3) {
+            console.log ("postal code less than 3 characters")
+        } else {
+            console.log ("ok")
+        }
+    }
+
+    identification.onblur = validateIdentification;
+
+    function validateIdentification () {
+        if (identification.value.search (/\d/) == -1) {
+            console.log ("type a number");
+        } else if (identification.value.length > 8 || identification.value.length < 7) {
+            console.log ("number between 7 and 8");
+        }   else if (identification.value % 1 !== 0) {
+            console.log ("identification must be an integer");
+        }
+    }
+
+    function hideDiv () {
+        document.getElementById("fullnameoutput").style.visibility = "hidden";
+        document.getElementById("emailoutput").style.visibility = "hidden";
     }
 }
